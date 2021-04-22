083dc2769663dcbd8f0e22f54c001a80b5f8fa8c
Jesper Stemann Andersen <jsa@ihp.dk>
2021-04-22 11:56:37 +0200

    Added AbstractTypeSerializer and SerializationTypeSerializer

--- src/TypeSerializers.jl
+++ src/TypeSerializers.jl
@@ -1,3 +1,22 @@
 module TypeSerializers
 
+import Serialization: serialize, deserialize
+
+export AbstractTypeSerializer, SerializationTypeSerializer, serialize, deserialize
+
+abstract type AbstractTypeSerializer{T} end
+
+abstract type SerializationTypeSerializer{T} <: AbstractTypeSerializer{T} end
+
+function serialize(::Type{TSerializer}, value::T)::IO where {T, TSerializer <: SerializationTypeSerializer{T}}
+    stream = IOBuffer(UInt8[], read = true, write = true)
+    serialize(stream, value)
+    return stream
 end
+
+function deserialize(::Type{TSerializer}, stream::IO)::T where {T, TSerializer <: SerializationTypeSerializer{T}}
+    value = deserialize(stream)
+    return value
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,24 @@
+using TypeSerializers
 using Test
 
+struct Foo
+    i::Int
+    s::String
+end
+
 @testset "TypeSerializers" begin
+    integer_value = 42
+    stream = serialize(SerializationTypeSerializer{Int}, integer_value)
+    seekstart(stream)
+    @test deserialize(SerializationTypeSerializer{Int}, stream) == integer_value
+
+    string_value = "Hello World!"
+    stream = serialize(SerializationTypeSerializer{String}, string_value)
+    seekstart(stream)
+    @test deserialize(SerializationTypeSerializer{String}, stream) == string_value
+
+    foo_value = Foo(integer_value, string_value)
+    stream = serialize(SerializationTypeSerializer{Foo}, foo_value)
+    seekstart(stream)
+    @test deserialize(SerializationTypeSerializer{Foo}, stream) == foo_value
 end
